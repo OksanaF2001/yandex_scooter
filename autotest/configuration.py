2057794168c5c64a20a4d87da9cdd9ae928e4fba
Filatova Oksana <fitatova.oksanaa@yandex.ru>
2023-06-07 07:20:42 +0300

    scrin

--- autotest/configuration.py
+++ autotest/configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://34a32ad4-d48b-4fbe-8c6b-47741f64b9f8.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://b3c2b938-1839-4058-b31b-85a895c706a0.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER_BY_TRACK_NUMBER = "/api/v1/orders/track?t=%d"
